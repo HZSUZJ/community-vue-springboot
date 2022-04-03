2b1f62edaacb0ecda53e8c320d59031025fb04d2
SUZJ <905430093@qq.com>
2022-04-03 18:33:36 +0800

    发布问题支持Markdown，实现上传图片功能

---
+++ back/src/main/java/com/su/community/controller/FileController.java
@@ -0,0 +1,22 @@
+package com.su.community.controller;
+
+import com.alibaba.fastjson.JSON;
+import com.su.community.dto.FileDTO;
+import lombok.Data;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class FileController {
+    @ResponseBody
+    @RequestMapping("/file/upload")
+    public String upload(){
+        FileDTO fileDTO=new FileDTO();
+        fileDTO.setSuccess(1);
+        fileDTO.setUrl("/images/loading.gif");
+        return JSON.toJSONString(fileDTO);
+    }
+
+}
