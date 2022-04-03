43dc14f0c9d7ddc5275412d8b535a04630759dfa
SUZJ <905430093@qq.com>
2022-04-03 20:48:17 +0800

    实现云存储上传图片

--- back/src/main/java/com/su/community/controller/FileController.java
+++ back/src/main/java/com/su/community/controller/FileController.java
@@ -1,21 +1,44 @@
 package com.su.community.controller;
 
 import com.alibaba.fastjson.JSON;
+import com.qcloud.cos.COSClient;
+import com.qcloud.cos.ClientConfig;
+import com.qcloud.cos.auth.BasicCOSCredentials;
+import com.qcloud.cos.auth.COSCredentials;
+import com.qcloud.cos.exception.CosClientException;
+import com.qcloud.cos.exception.CosServiceException;
+import com.qcloud.cos.http.HttpProtocol;
+import com.qcloud.cos.model.*;
+import com.qcloud.cos.region.Region;
 import com.su.community.dto.FileDTO;
+import com.su.community.provider.TXCloudProvider;
 import lombok.Data;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.File;
+import java.io.IOException;
 
 @Controller
 public class FileController {
+
+    @Autowired
+    private TXCloudProvider txCloudProvider;
     @ResponseBody
     @RequestMapping("/file/upload")
-    public String upload(){
+    public String upload(HttpServletRequest request) throws IOException {
+        MultipartHttpServletRequest multipartHttpServletRequest= (MultipartHttpServletRequest) request;
+        MultipartFile file=multipartHttpServletRequest.getFile("editormd-image-file");
+        String url= txCloudProvider.upload(file.getInputStream(),file.getOriginalFilename(),file.getSize());
         FileDTO fileDTO=new FileDTO();
         fileDTO.setSuccess(1);
-        fileDTO.setUrl("/images/loading.gif");
+        fileDTO.setUrl(url);
         return JSON.toJSONString(fileDTO);
     }
 
---
+++ back/src/main/java/com/su/community/provider/TXCloudProvider.java
@@ -0,0 +1,81 @@
+package com.su.community.provider;
+
+import com.qcloud.cos.COSClient;
+import com.qcloud.cos.ClientConfig;
+import com.qcloud.cos.auth.AnonymousCOSCredentials;
+import com.qcloud.cos.auth.BasicCOSCredentials;
+import com.qcloud.cos.auth.COSCredentials;
+import com.qcloud.cos.exception.CosClientException;
+import com.qcloud.cos.exception.CosServiceException;
+import com.qcloud.cos.model.*;
+import com.qcloud.cos.region.Region;
+import com.qcloud.cos.transfer.TransferManager;
+import com.qcloud.cos.transfer.TransferManagerConfiguration;
+import com.qcloud.cos.transfer.Upload;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.net.URL;
+import java.util.Date;
+import java.util.UUID;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+@Service
+public class TXCloudProvider {
+    private String secretId="";
+    private String secretKey="";
+
+    COSCredentials cred = new BasicCOSCredentials(secretId, secretKey);
+    Region region = new Region("ap-guangzhou");
+    ClientConfig clientConfig = new ClientConfig(region);
+    String bucketName = "community-1254053068";
+
+    public String upload(InputStream fileStream,String filename,long inputStreamLength){
+        String generateFilename="";
+        String[] filePaths=filename.split("\\.");
+        if(filePaths.length>1){
+            generateFilename= UUID.randomUUID().toString()+"."+filePaths[filePaths.length-1];
+        }
+        TransferManager transferManager = createTransferManager();
+        String key = generateFilename;
+
+        ObjectMetadata objectMetadata = new ObjectMetadata();
+        objectMetadata.setContentLength(inputStreamLength);
+        PutObjectRequest putObjectRequest = new PutObjectRequest(bucketName, key, fileStream, objectMetadata);
+        try {
+            Upload upload = transferManager.upload(putObjectRequest);
+            UploadResult uploadResult = upload.waitForUploadResult();
+        } catch (CosServiceException e) {
+            e.printStackTrace();
+            return null;
+        } catch (CosClientException e) {
+            e.printStackTrace();
+            return null;
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+            return null;
+        }
+        shutdownTransferManager(transferManager);
+        COSClient cosclient = new COSClient(cred, clientConfig);
+        URL url= cosclient.getObjectUrl(bucketName, key);
+        return url.toString();
+    }
+
+    // 创建 TransferManager 实例，这个实例用来后续调用高级接口
+    TransferManager createTransferManager() {
+        COSClient cosClient = new COSClient(cred, clientConfig);
+        ExecutorService threadPool = Executors.newFixedThreadPool(32);
+        TransferManager transferManager = new TransferManager(cosClient, threadPool);
+        TransferManagerConfiguration transferManagerConfiguration = new TransferManagerConfiguration();
+        transferManagerConfiguration.setMultipartUploadThreshold(5*1024*1024);
+        transferManagerConfiguration.setMinimumUploadPartSize(1*1024*1024);
+        transferManager.setConfiguration(transferManagerConfiguration);
+        return transferManager;
+    }
+    void shutdownTransferManager(TransferManager transferManager) {
+        transferManager.shutdownNow(true);
+    }
+}
