35716be8cbe65e6d63a5ff53a5d4e4982d4a3367
SUZJ <905430093@qq.com>
2022-05-09 21:50:34 +0800

    add

---
+++ vue/src/main.js
@@ -0,0 +1,60 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import router from './router'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+import App from './App'
+import VueMarkdownEditor from '@kangc/v-md-editor';
+import '@kangc/v-md-editor/lib/style/base-editor.css';
+import vuepressTheme from '@kangc/v-md-editor/lib/theme/vuepress.js';
+import VMdPreview from '@kangc/v-md-editor/lib/preview';
+import '@kangc/v-md-editor/lib/style/preview.css';
+import githubTheme from '@kangc/v-md-editor/lib/theme/github.js';
+import '@kangc/v-md-editor/lib/theme/style/github.css';
+
+// highlightjs
+import hljs from 'highlight.js';
+
+VMdPreview.use(githubTheme, {
+  Hljs: hljs,
+});
+
+Vue.use(VMdPreview);
+
+
+VueMarkdownEditor.use(vuepressTheme);
+
+Vue.use(VueMarkdownEditor);
+
+Vue.config.productionTip = false
+
+axios.defaults.baseURL = "http://10.12.11.142:8081"
+Vue.use(VueAxios, axios)
+Vue.use(ElementUI)
+/* eslint-disable no-new */
+
+axios.interceptors.request.use(config => {
+  // 为请求头添加Authorization字段为服务端返回的token
+  config.headers.token = localStorage.getItem('token')
+  // return config是固定用法 必须有返回值
+  return config
+})
+//假设token过期
+axios.interceptors.response.use(res => {
+  let data = res.data
+  if (res.data.code == 50000) {
+    localStorage.removeItem('token')
+  }
+  return data
+})
+
+new Vue({
+  el: '#app',
+  router,
+  components: {App},
+  template: '<App/>',
+  render: h => h(App)
+})
---
+++ vue/src/router/index.js
@@ -0,0 +1,187 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Main from '../views/Main'
+import Login from '../views/Login'
+import NotFound from '../views/NotFound'
+import BoardList from "../views/BoardList"
+import NewTopics from "../views/NewTopics"
+import Focus from "../views/Focus"
+import Board from "../views/focus/Board";
+import User from "../views/focus/User";
+import Favorite from "../views/focus/Favorite";
+import Message from "../views/Message";
+import Response from "../views/message/Response";
+import Attme from "../views/message/Attme";
+import System from "../views/message/System";
+import DirectMessages from "../views/message/DirectMessages";
+import UserCenter from "../views/UserCenter";
+import Detail from "../views/usercenter/Detail";
+import Config from "../views/usercenter/Config";
+import MyTopics from "../views/usercenter/MyTopics";
+import MyPosts from "../views/usercenter/MyPosts";
+import MyFavorites from "../views/usercenter/MyFavorites";
+import MyFollowings from "../views/usercenter/MyFollowings";
+import MyFans from "../views/usercenter/MyFans";
+import PostTopic from "../views/PostTopic";
+import TopicDetail from '../views/TopicDetail'
+
+Vue.use(Router)
+const router = new Router({
+  mode: 'history',
+  routes: [
+    {
+      path: '/',
+      name: 'main',
+      component: Main
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: Login
+    },
+    {
+      path: '/boardList',
+      name: 'boardList',
+      component: BoardList
+    },
+    {
+      path: '/newTopics',
+      name: 'newTopics',
+      component: NewTopics
+    },
+    {
+      path: '/focus',
+      name: 'focus',
+      component: Focus,
+      children: [
+        {
+          path: '/focus/board',
+          name: 'board',
+          component: Board
+        },
+        {
+          path: '/focus/user',
+          name: 'user',
+          component: User
+        },
+        {
+          path: '/focus/favorite',
+          name: 'favorite',
+          component: Favorite
+        }
+      ]
+    },
+    {
+      path: '/message',
+      name: 'message',
+      component: Message,
+      children: [
+        {
+          path: '/message/response',
+          name: 'response',
+          component: Response
+        },
+        {
+          path: '/message/attme',
+          name: 'attme',
+          component: Attme
+        },
+        {
+          path: '/message/system',
+          name: 'system',
+          component: System
+        },
+        {
+          path: '/message/directMessages',
+          name: 'directMessages',
+          component: DirectMessages
+        }
+      ]
+    },
+    {
+      path: '/usercenter',
+      name: 'usercenter',
+      component: UserCenter,
+      children: [
+        {
+          path: '/usercenter/detail',
+          name: 'detail',
+          component: Detail
+        },
+        {
+          path: '/usercenter/config',
+          name: 'config',
+          component: Config
+        },
+        {
+          path: '/usercenter/mytopics',
+          name: 'mytopics',
+          component: MyTopics
+        },
+        {
+          path: '/usercenter/myposts',
+          name: 'myposts',
+          component: MyPosts
+        },
+        {
+          path: '/usercenter/myfavorites',
+          name: 'myfavorites',
+          component: MyFavorites
+        },
+        {
+          path: '/usercenter/myfocusboards',
+          name: 'myfocusboards',
+          component: MyFavorites
+        },
+        {
+          path: '/usercenter/myfollowings',
+          name: 'myfollowings',
+          component: MyFollowings
+        },
+        {
+          path: '/usercenter/myfans',
+          name: 'myfans',
+          component: MyFans
+        }
+      ]
+    },
+    {
+      path: '/postTopic',
+      name: 'PostTopic',
+      component: PostTopic
+    },
+    {
+      path: '/topic/:id',
+      name: 'TopicDetail',
+      component: TopicDetail
+    },
+    {
+      path: '/gohome',
+      redirect: '/main'
+    },
+    {
+      path: '*',
+      component: NotFound
+    }
+  ]
+})
+// 使用 router.beforeEach 注册一个全局前置守卫，判断用户是否登陆
+router.beforeEach((to, from, next) => {
+  if (to.path === '/register' || to.path === '/login') {
+    next()
+  } else {
+    let token = localStorage.getItem('token')
+    if (token === null || token === '') {
+      next('/login')
+    } else {
+      next()
+    }
+  }
+})
+const VueRouterPush = Router.prototype.push
+Router.prototype.push = function push(to) {
+  return VueRouterPush.call(this, to).catch(err => err)
+}
+
+export default router
+
