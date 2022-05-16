de35eaeecec3428f42b90a082222bfed262c0f6a
xiangyu <2084921287@qq.com>
2022-05-16 17:34:35 +0800

    详情页

--- vue/src/views/CommentEntry.vue
+++ vue/src/views/CommentEntry.vue
@@ -1,63 +1,92 @@
 <template>
   <div>
-    <el-row
-      style="padding: 10px;margin-top: 20px;border: 1px;box-shadow: 0 2px 4px rgba(0, 0, 0, .12), 0 0 6px rgba(0, 0, 0, .04)">
-      <el-col :span="6" style="background-color: #F2F6FC">
-        <div class="grid-content">
-          <el-container>
-            <el-aside width="50px">
+    <el-row class="msg_menu">
+      <el-container>
+        <el-aside width="250px" style="background-color: #79b8ca">
+          <div class="userMessage" style="margin-top: 24px;">
+            <div class="userMessage-left" style="float:left; width: 76px; margin-top:16px; padding-left: 24px;">
+              <el-row style="text-align: left; font-size: 20px">{{ comment.user.username }}</el-row>
+            </div>
+            <div class="userMessage-right" style="float:right; width: 94px; padding-right: 10px;">
               <el-avatar :src=comment.user.avatarUrl></el-avatar>
-            </el-aside>
-            <el-main>{{ comment.user.username }}</el-main>
-          </el-container>
-        </div>
-      </el-col>
-      <el-col :span="12">
-        <div class="grid-content">
+              <div class="userMessageBtn" style="margin-top: 20px">
+                <a href="#"><button class="replierBtn" id="follow" style="float: left">关注</button></a>
+                <a href="#"><button class="replierBtn" style="float: right">私信</button></a>
+              </div>
+            </div>
+          </div>
+                <!--              <el-avatar :src=comment.user.avatarUrl></el-avatar>-->
+        </el-aside>
+
+        <el-main :src=comment.user.avatarUrl>
           <el-row style="text-align: left;padding-left: 15px">
             <v-md-preview :text="comment.content"></v-md-preview>
           </el-row>
           <el-row style="text-align: left">
             <i class="el-icon-time" style="margin-right: 10px">{{ comment.gmtCreate | formatDate }}</i>
           </el-row>
-        </div>
-      </el-col>
-
+        </el-main>
+      </el-container>
     </el-row>
   </div>
 </template>
 
 <script>
 export default {
-  name: "CommentEntry",
+  name: 'CommentEntry',
   props: ['comment'],
   filters: {
     formatDate: function (value) {
-      let curdate = new Date().getTime();
-      let dffdate = (curdate - value) / 1000 / 60;
+      let curdate = new Date().getTime()
+      let dffdate = (curdate - value) / 1000 / 60
       if (dffdate < 60) {
-        if (parseInt(dffdate) === 0)
-          return '刚刚';
-        return parseInt(dffdate) + '分钟前';
+        if (parseInt(dffdate) === 0) { return '刚刚' }
+        return parseInt(dffdate) + '分钟前'
       }
-      let date = new Date(value);
-      let y = date.getFullYear();
-      let MM = date.getMonth() + 1;
-      MM = MM < 10 ? ('0' + MM) : MM;
-      let d = date.getDate();
-      d = d < 10 ? ('0' + d) : d;
-      let h = date.getHours();
-      h = h < 10 ? ('0' + h) : h;
-      let m = date.getMinutes();
-      m = m < 10 ? ('0' + m) : m;
-      let s = date.getSeconds();
-      s = s < 10 ? ('0' + s) : s;
-      return y + '-' + MM + '-' + d + ' ' + h + ':' + m + ':' + s;
+      let date = new Date(value)
+      let y = date.getFullYear()
+      let MM = date.getMonth() + 1
+      MM = MM < 10 ? ('0' + MM) : MM
+      let d = date.getDate()
+      d = d < 10 ? ('0' + d) : d
+      let h = date.getHours()
+      h = h < 10 ? ('0' + h) : h
+      let m = date.getMinutes()
+      m = m < 10 ? ('0' + m) : m
+      let s = date.getSeconds()
+      s = s < 10 ? ('0' + s) : s
+      return y + '-' + MM + '-' + d + ' ' + h + ':' + m + ':' + s
     }
   }
 }
 </script>
 
 <style scoped>
+
+.msg_menu {
+  margin-top: 20px;
+  border: 2px solid #79b8ca;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, .12), 0 0 6px rgba(0, 0, 0, .04)
+}
+
+.el-col-7 {
+  width: 274px;
+  /*background-color: #F2F6FC;*/
+  background-color: #79b8ca;
+  /*background-color: pink;*/
+
+}
+
+button {
+  background-color: #79b8ca;
+  color: #fff;
+  border: #fff solid thin;
+  border-radius: 0.63rem;
+  width: 2.6rem;
+  height: 1.4rem;
+  cursor: pointer;
+  font-size: .6rem;
+  overflow: visible;
+}
 
 </style>
--- vue/src/views/TopicDetail.vue
+++ vue/src/views/TopicDetail.vue
@@ -1,7 +1,15 @@
 <template>
   <div>
-    <div id="nt-root" style="margin-top: 50px">
-      <div class="el-row">
+    <div id="nt-root" style="margin-top: 15px">
+      <div class="el-row-fenye" style="width: 965px; margin:0 auto;">
+        <el-pagination style="display:block; float: right"
+                       background
+                       layout="prev, pager, next"
+                       :page-size="10"
+                       :current-page=currentPage
+                       :total=total
+                       @current-change="changePage">
+        </el-pagination>
       </div>
       <div id="tp-area">
         <el-row>
@@ -11,33 +19,26 @@
           <!--          帖子标题-->
           <el-col :span="12">
             <div class="grid-content">
-              <el-row
-                style="padding: 10px;border: 1px;box-shadow: 0 2px 4px rgba(0, 0, 0, .12), 0 0 6px rgba(0, 0, 0, .04)">
+
+              <el-row class="el-row-tiezi">
                 <el-col :span="12">
                   <div class="grid-content">
-                    <el-row style="text-align: left;padding-left: 15px">
+                    <el-row style="height: 40px;line-height: 40px;text-align: left;padding-left: 15px; margin-bottom: 5px">
                       {{ topic.title }}
                     </el-row>
                     <el-row style="text-align: left">
-                      <i class="el-icon-time" style="margin-right: 10px">{{ topic.gmtCreate | formatDate }}</i>
-                      <i class="el-icon-view" style="margin-right: 10px">5</i>
+                      <i class="el-icon-time" style="margin-right: 10px; color: #79b8ca">{{ topic.gmtCreate | formatDate }}</i>
+                      <i class="el-icon-view" style="margin-right: 10px; color: #79b8ca">5</i>
                     </el-row>
                   </div>
                 </el-col>
                 <el-col :span="4">
-                  <div class="grid-content">
+                  <div class="grid-content ">
                     <span>{{ topic.board }}</span>
                   </div>
                 </el-col>
               </el-row>
-              <el-pagination style="float: right"
-                             background
-                             layout="prev, pager, next"
-                             :page-size="10"
-                             :current-page=currentPage
-                             :total=total
-                             @current-change="changePage">
-              </el-pagination>
+
               <!--              内容区-->
               <commentEntry v-if="currentPage==1" :comment="topic"></commentEntry>
               <!--              评论区-->
@@ -60,7 +61,6 @@
             </div>
           </el-col>
 
-
           <el-col :span="6">
             <div class="grid-content bg-purple"></div>
           </el-col>
@@ -74,8 +74,8 @@
 import commentEntry from './CommentEntry'
 
 export default {
-  name: "TopicDetail",
-  data() {
+  name: 'TopicDetail',
+  data () {
     return {
       topic: '',
       content: '',
@@ -85,7 +85,7 @@ export default {
     }
   },
   methods: {
-    changePage(val) {
+    changePage (val) {
       this.currentPage = val
       this.axios.get(`/getAllComment/${this.topic.id}/${val}`).then(res => {
         if (res.data.code === 200) {
@@ -94,9 +94,8 @@ export default {
       }).catch(e => {
         alert('服务器故障')
       })
-
     },
-    onUploadImage(event, insertImage, files) {
+    onUploadImage (event, insertImage, files) {
       const file = files[0]
       let param = new FormData()
       param.append('file', file)
@@ -111,18 +110,18 @@ export default {
         alert('图片上传出了点小问题，请稍后重试')
       })
     },
-    onSubmit() {
+    onSubmit () {
       let param = new FormData()
-      param.append("topicId", this.topic.id)
-      param.append("parentId", 0)
-      param.append("content", this.content)
+      param.append('topicId', this.topic.id)
+      param.append('parentId', 0)
+      param.append('content', this.content)
       this.axios.post(`/postComment`, param).then(res => {
         if (res.data.code === 200) {
           this.$message({
             message: '登录成功',
             type: 'success'
-          });
-          const that = this;
+          })
+          const that = this
           setTimeout(function () {
             that.$router.go(0)
           }, 1000)
@@ -132,9 +131,9 @@ export default {
       })
     }
   },
-  created() {
+  created () {
     let param = new FormData()
-    param.append("id", this.$route.params.id)
+    param.append('id', this.$route.params.id)
     this.axios.get(`/getTopicDetail/${this.$route.params.id}`).then(res => {
       if (res.data.code === 200) {
         this.topic = res.data.data
@@ -158,37 +157,33 @@ export default {
     }).catch(e => {
       alert('服务器故障')
     })
-
-
   },
   components: {
     commentEntry: commentEntry
   },
   filters: {
     formatDate: function (value) {
-      let curdate = new Date().getTime();
-      let dffdate = (curdate - value) / 1000 / 60;
+      let curdate = new Date().getTime()
+      let dffdate = (curdate - value) / 1000 / 60
       if (dffdate < 60) {
-        if (parseInt(dffdate) === 0)
-          return '刚刚';
-        return parseInt(dffdate) + '分钟前';
+        if (parseInt(dffdate) === 0) { return '刚刚' }
+        return parseInt(dffdate) + '分钟前'
       }
-      let date = new Date(value);
-      let y = date.getFullYear();
-      let MM = date.getMonth() + 1;
-      MM = MM < 10 ? ('0' + MM) : MM;
-      let d = date.getDate();
-      d = d < 10 ? ('0' + d) : d;
-      let h = date.getHours();
-      h = h < 10 ? ('0' + h) : h;
-      let m = date.getMinutes();
-      m = m < 10 ? ('0' + m) : m;
-      let s = date.getSeconds();
-      s = s < 10 ? ('0' + s) : s;
-      return y + '-' + MM + '-' + d + ' ' + h + ':' + m + ':' + s;
+      let date = new Date(value)
+      let y = date.getFullYear()
+      let MM = date.getMonth() + 1
+      MM = MM < 10 ? ('0' + MM) : MM
+      let d = date.getDate()
+      d = d < 10 ? ('0' + d) : d
+      let h = date.getHours()
+      h = h < 10 ? ('0' + h) : h
+      let m = date.getMinutes()
+      m = m < 10 ? ('0' + m) : m
+      let s = date.getSeconds()
+      s = s < 10 ? ('0' + s) : s
+      return y + '-' + MM + '-' + d + ' ' + h + ':' + m + ':' + s
     }
   }
-
 
 }
 </script>
@@ -197,6 +192,20 @@ export default {
 .grid-content {
   border-radius: 4px;
   min-height: 36px;
+}
+
+.el-row-tiezi {
+  margin-top: 10px;
+  padding: 10px;
+  border: 2px solid #808080;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, .12), 0 0 6px rgba(0, 0, 0, .04);
+}
+
+.el-col-4 {
+  height: 40px;;
+  line-height: 40px;
+  margin-top: 11px;
+  border-left: 1px solid #333333;
 }
 
 </style>
