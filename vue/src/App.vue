9224d3fb22e755b87b65db96bcfe58233b457a7f
= <1317964417@qq.com>
2022-09-14 21:30:27 +0800

    v1.0.0

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,12 +1,12 @@
 <template>
-  <div id="app">
-    <el-container>
-      <el-header>
-        <nav-menu></nav-menu>
-      </el-header>
-    </el-container>
-    <router-view/>
-  </div>
+    <div id="app">
+        <el-container>
+            <el-header>
+                <nav-menu></nav-menu>
+            </el-header>
+        </el-container>
+        <router-view />
+    </div>
 </template>
 
 <script>
@@ -14,19 +14,19 @@ import navMenu from "./components/NavMenu"
 
 
 export default {
-  name: 'App',
-  components: {
-    navMenu: navMenu
-  }
+    name: 'App',
+    components: {
+        navMenu: navMenu
+    }
 }
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
 }
 </style>
--- vue/src/main.js
+++ vue/src/main.js
@@ -18,7 +18,7 @@ import '@kangc/v-md-editor/lib/theme/style/github.css';
 import hljs from 'highlight.js';
 
 VMdPreview.use(githubTheme, {
-  Hljs: hljs,
+    Hljs: hljs,
 });
 
 Vue.use(VMdPreview);
@@ -30,31 +30,31 @@ Vue.use(VueMarkdownEditor);
 
 Vue.config.productionTip = false
 
-// axios.defaults.baseURL = "http://10.12.11.142:8081"
-axios.defaults.baseURL = "http://localhost:8081"
+axios.defaults.baseURL = "http://10.12.0.146:8081"
+// axios.defaults.baseURL = "http://localhost:8081"
 Vue.use(VueAxios, axios)
 Vue.use(ElementUI)
 /* eslint-disable no-new */
 
 axios.interceptors.request.use(config => {
-  // 为请求头添加Authorization字段为服务端返回的token
-  config.headers.token = localStorage.getItem('token')
-  // return config是固定用法 必须有返回值
-  return config
+    // 为请求头添加Authorization字段为服务端返回的token
+    config.headers.token = localStorage.getItem('token')
+    // return config是固定用法 必须有返回值
+    return config
 })
 //假设token过期
 axios.interceptors.response.use(res => {
-  let data = res.data
-  if (res.data.code === 50000) {
-    localStorage.removeItem('token')
-  }
-  return data
+    let data = res.data
+    if (res.data.code === 50000) {
+        localStorage.removeItem('token')
+    }
+    return data
 })
 
 new Vue({
-  el: '#app',
-  router,
-  components: {App},
-  template: '<App/>',
-  render: h => h(App)
+    el: '#app',
+    router,
+    components: { App },
+    template: '<App/>',
+    render: h => h(App)
 })
